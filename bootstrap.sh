34c4881c51973f1ce18a79f8b3576fd1aad5f999
Nate Eizenga <eizengan@gmail.com>
2021-01-28 15:19:31 -0500

    Interactive setup, prepare .env early, use pry

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,41 @@
 #!/bin/sh
-set -eux
+set -eu
 
 # TODO:
-# test for existing directory
-# check for compose upfront
-# forward args to rails new
+# ci, heroku
+# include setup scripting
+# consolidate docker files
+# stop using database.yml, use DATABASE_URL instead
+
+if ! command -v docker-compose &> /dev/null
+then
+  echo "the docker-compose command is required to run this script"
+  exit 1
+fi
+
+echo "Enter the name of the application (lowercase, snakecase, not an existing directory)"
+read -p "application name: " application_name
+if [ -z "$application_name" ]
+then
+  echo "application name is not optional"
+  exit 1
+fi
+if [ -d "$application_name" ]
+then
+  echo "directory already exists"
+  exit 1
+fi
+
+
+echo "Press return to generate a strong password for Postgres, or enter one if desired:"
+read -p "password: " postgres_password
+if [ -z $postgres_password ]
+then
+  echo "generating a password..."
+  postgres_password="$(cat /dev/urandom | base64 | tr -cd "[:upper:][:lower:][:digit:]" | head -c 32)"
+fi
 
 # Set up workspace
-application_name=$1
 mkdir $application_name
 cd $application_name
 
@@ -29,16 +57,23 @@ curl -LJO https://raw.githubusercontent.com/mhs/docker-rails-bootstrapper/main/s
 curl -LJO https://raw.githubusercontent.com/mhs/docker-rails-bootstrapper/main/support/docker-compose.override.yml
 sed -i.bkp "s/<APPLICATION_NAME>/$application_name/g" docker-compose.yml
 sed -i.bkp "s/<APPLICATION_NAME>/$application_name/g" docker-compose.override.yml
-touch ./sample.docker.bashrc ./sample.docker.bash_history
-touch ./docker.bashrc ./docker.bash_history
+touch ./sample.docker.bashrc ./sample.docker.bash_history ./sample.docker.pry_history
+touch ./docker.bashrc ./docker.bash_history ./docker.pry_history
 
 # append to .gitignore
 curl -L https://raw.githubusercontent.com/mhs/docker-rails-bootstrapper/main/support/additions.gitignore >> .gitignore
 
+# create, populate .env
+curl -LJO https://raw.githubusercontent.com/mhs/docker-rails-bootstrapper/main/support/.env
+cp .env sample.env
+sed -i.bkp "s/<POSTGRES_PASSWORD>/$postgres_password/g" .env
+
 # set up default gems
 echo "
 # TODO: move automatically-installed gems to the appropriate blocks:" >> Gemfile
-docker-compose run web bundle add --group development,test rspec-rails \
+docker-compose run web bundle add pry-rails
+docker-compose run web bundle add --group development,test pry-byebug \
+                                                           rspec-rails \
                                                            factory_bot_rails \
                                                            rubocop-rails \
                                                            rubocop-rails_config rubocop-performance rubocop-rspec \
@@ -52,21 +87,12 @@ curl -Lo lib/tasks/rubocop.rake https://raw.githubusercontent.com/mhs/docker-rai
 # this uncomments the corresponding line in spec/rails_helper.rb
 sed -i.bkp "s/# \(Dir\[Rails.root.join('spec', 'support'\)/\1/g" spec/rails_helper.rb
 
-# create, populate .env
-curl -LJO https://raw.githubusercontent.com/mhs/docker-rails-bootstrapper/main/support/.env
-echo "Press return to generate a strong password for Postgres, or enter one if desired:"
-read -p "password: " postgres_password
-if [ -z $postgres_password ]
-then
-  echo "generating a password..."
-  postgres_password="$(cat /dev/urandom | base64 | tr -cd "[:upper:][:lower:][:digit:]" | head -c 32)"
-fi
-sed -i.bkp "s/<POSTGRES_PASSWORD>/$postgres_password/g" .env
-
 # migrate the database and run setup
+# TODO: use DATABASE_URL instead of overriding database.yml
+# - postgres://postgres:${POSTGRES_PASSWORD}@db:5432/<app_name>_<env> ???
 curl -Lo config/database.yml https://raw.githubusercontent.com/mhs/docker-rails-bootstrapper/main/support/database.yml
 sed -i.bkp "s/<APPLICATION_NAME>/$application_name/g" config/database.yml
 docker-compose run web bundle exec rails db:create db:migrate
 
 # cleanup
-rm -f **/*.bkp docker-compose.bootstrap.yml test lib/tasks/.keep
+rm -rf **/*.bkp docker-compose.bootstrap.yml test lib/tasks/.keep
